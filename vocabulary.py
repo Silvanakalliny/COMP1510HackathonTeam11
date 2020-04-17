3260dff8da5c3f20f9d65037f1f54d30ea3de1a9
Silvana <skalliny@my.bcit.ca>
2020-04-17 13:42:56 -0400

    edited vocabulary class

--- vocabulary.py
+++ vocabulary.py
@@ -3,18 +3,18 @@ class Vocabulary:
         self.__words = set()
 
     def add_a_word(self, new_word):
-        self.__words.add(new_word)
+        self.__words.add(new_word.lower().strip())
 
     def add_words(self, *new_words):
         for word in new_words:
-            self.add_a_word(word)
+            self.add_a_word(word.lower().strip())
 
     def remove_a_word(self, word):
-        self.__words.remove(word)
+        self.__words.remove(word.lower().strip())
 
     def remove_words(self, *words):
         for word in words:
-            self.remove_a_word(word)
+            self.remove_a_word(word.lower().strip())
 
     def sort_words(self):
         return sorted(list(self.__words))
