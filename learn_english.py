19b6adffbefeab5103b3044af4b0aac0f11f7015
Kyle Bulalakaw <kbulalakaw2@my.bcit.ca>
2020-04-17 11:14:49 -0700

    added import time statement and study_time function to learn_english.py

--- learn_english.py
+++ learn_english.py
@@ -1,2 +1,20 @@
+import time
+
+
+def study_time(func) -> float:
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        hours = int(run_time // 3600)
+        minutes = int(run_time % 3600 // 60)
+        seconds = int(run_time % 3600 % 60)
+        print(f"You studied for {hours} hours, {minutes} minutes and {seconds} seconds.")
+        return run_time  # removed {func.__name__!r} from above, may use it later to state lesson name or something
+    return wrapper_timer
+
+
+@study_time
 def main():
     pass
