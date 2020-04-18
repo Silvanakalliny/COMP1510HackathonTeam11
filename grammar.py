80f6b2d979afe8277f8667bf5af5487cd711588e
Silvana <skalliny@my.bcit.ca>
2020-04-17 22:18:48 -0400

    modified print_list function

--- grammar.py
+++ grammar.py
@@ -1,4 +1,13 @@
+import time
+import doctest
+
+
 def grammar_questions():
+    """
+    Return a dictionary that contains all the questions used for the grammar quiz.
+
+    :return: a dictionary that contains all the questions used for the grammar quiz.
+    """
     # Questions taken from https://www.esl-lounge.com/student/grammar-exercises-beginner.php
     question_options = {"capitals": {"description": "Find and type in the word that is not properly capitalized (either missing a capital "
                         "where there should be one, or containing a capital letter that should not be there.)",
@@ -28,41 +37,74 @@ def grammar_questions():
 
 
 def grammar_test():
-    test_type_input = int(input("""\nWould you like to test your knowledge of proper use of capitals, general verb conjugation or present simple verb conjugation?
-                         1. Capitals
+    """
+    Initialize the grammar quiz by allowing the user to input the type of exercise they'd like to do, and launching the
+    proper exercise.
+
+    :precondition: the user must input an integer between 1 and 3 when prompted.
+    :postcondition: the function will launch the quiz of the users choice.
+    :raise: ValueError, if the user's input is anything other than an integer between 1 and 3.
+    """
+    try:
+        test_type_input = int(input("""\nWould you like to test your knowledge of proper use of capitals, general verb conjugation or present simple verb conjugation?
+                         1. Capitalization
                          2. General Verb Conjugation
                          3. Present simple verb conjugation\n\nPlease enter a number between 1-3 to continue.\n"""))
-    test_type = ["capitals", "verbs", "present simple mc"]
-    question_source = grammar_questions()
-    chosen_test = test_type[test_type_input - 1]
-    score = 0
-    run_test(question_source, chosen_test, score)
+    except ValueError:
+        print("Invalid input. Please ensure you enter a number between 1-3.")
+        time.sleep(1)
+        grammar_test()
+    else:
+        test_type = ["capitals", "verbs", "present simple mc"]
+        question_source = grammar_questions()
+        chosen_test = test_type[test_type_input - 1]
+        run_test(question_source, chosen_test)
 
 
-def run_test(question_bank, test, score):
-    questions = question_bank[test]["questions"].keys()
-    test_description = question_bank[test]["description"]
-    print("\n{}\n".format(test_description))
+def run_test(question_bank, test):
+    """
+    Run the quiz by asking the user each question from the test bank for the activity they chose, and grading their answers.
+
+    :param question_bank: a dictionary which contains all the questions to be used for the quiz, as well as the answers.
+    :param test: a string which specifies the type of activity the user has selected.
+    :precondition: users must only enter valid values for their answers to the questions, as indicated in the prompts.
+    :precondition: the function will continue to ask questions and mark users until it has gone through all of the questions.
+
+    """
+    questions = question_bank[test]["questions"].keys()  # gets the list of questions for this activity
+    score = 0  # initial score
+    test_description = question_bank[test]["description"]  # gets the description/brief instructions of the activity
+    print("\n{}\n".format(test_description))  # prints the test description
     for n, question in enumerate(questions, 1):
-        print('%d. %s' % (n, question))
-        if test == "verbs":
+        print('%d. %s' % (n, question))  # prints the question and question number
+        if test == "verbs":  # if the test is a verb conjugation test, it prints out the two options: correct and incorrect
             print("""   1. Correct\n   2. Incorrect""")
-        elif test == "present simple mc":
+        elif test == "present simple mc":  # if the user is doing the present simple test, the three MC options are printed to the user
             for num, answer in enumerate(question_bank[test]["questions"][question][1], 1):
                 print('     %d. %s' % (num, answer))
-        user_answer = input().strip().lower()
-        user_answer = remove_punctuation(user_answer)
-        if test == "capitals":
+        user_answer = input().strip().lower()  # cleans up the input
+        user_answer = remove_punctuation(user_answer)  # removes all punctuation from the input
+        if test == "capitals":  # selects the appropriate 'grading' function depending on what type of quiz it is
             score += check_answer_capitals(question_bank, question, user_answer)
         elif test == "verbs":
             score += check_answer_verbs(question_bank, question, user_answer)
         else:
             score += check_answer_present_simple(question_bank, question, user_answer)
-    possible_score = len(question_bank[test]["questions"].keys())
-    print(f"Your final score was {score} out of a possible {possible_score}.")
+    possible_score = len(question_bank[test]["questions"].keys())  # determines the highest possible score for that quiz
+    print(f"Your final score was {score} out of a possible {possible_score}.")  # prints the user's actual score and the highest possible score.
 
 
 def remove_punctuation(answer):
+    """
+    Remove all punctuation from the string 'answer' and return 'answer' with all punctuation removed
+    :param answer: a string, from which all punctuation will be removed
+    :return: 'answer,' a version of the string 'answer' with all punctuation removed
+
+    >>> remove_punctuation("Dr.")
+    'Dr'
+    >>> remove_punctuation("hello_")
+    'hello'
+    """
     punctuation = ',.~!@#$%^&*()_+-={}[]|/<>:’\“;\"\'\n'
     for marker in punctuation:
         if marker in answer:
@@ -71,44 +113,90 @@ def remove_punctuation(answer):
 
 
 def check_answer_capitals(question_source, question, answer):
+    """
+    Checks the answer if the user is doing a capitalization quiz.
+
+    :param question_source: a dictionary which contains the questions referenced for the quiz.
+    :param question: a string, the question whose answer is currently being 'graded'.
+    :param answer: a string, the answer the user has given to be 'graded'.
+    :return: the score for that question, an integer.
+    """
     score = 0
-    correct_answer = question_source["capitals"]["questions"][question]
+    correct_answer = question_source["capitals"]["questions"][question]  # retrieves the correct answer
     if answer == correct_answer:
-        print("Correct!")
+        print("Correct!\n")
+        time.sleep(1)
         score += 1
     else:
-        print(f"Wrong answer.The correct answer is {correct_answer}.")
+        print(f"Wrong answer.The correct answer is {correct_answer}.\n")
+        time.sleep(1)
     return score
 
 
 def check_answer_verbs(question_source, question, answer):
+    """
+    Checks the answer if the user is doing a verb conjugation quiz.
+
+    :param question_source: a dictionary which contains the questions referenced for the quiz.
+    :param question: a string, the question whose answer is currently being 'graded'.
+    :param answer: a string, the answer the user has given to be 'graded'.
+    :return: the score for that question, an integer.
+    """
     score = 0
     possible_answers = ["Correct", "Incorrect"]
-    correct_answer = question_source["verbs"]["questions"][question]
-    answer = int(answer)
-    if answer != 1 and answer != 2:
-        raise ValueError
-    elif possible_answers[answer - 1] == correct_answer:
-        print("Correct!")
-        score += 1
+    correct_answer = question_source["verbs"]["questions"][question]  # retrieves the correct answer
+    try:  # code that could raise a Value Error
+        answer = int(answer)
+        if answer != 1 and answer != 2:
+            raise ValueError
+    except ValueError:  # excepts the error
+        print("Invalid input. Please enter either 1 or 2.")
+        time.sleep(1)
+        answer = input("Enter a valid input.").strip().lower()
+        return check_answer_verbs(question_source, question, answer)
     else:
-        print(f"Wrong answer.The correct answer is {correct_answer}.")
-    return score
+        if possible_answers[answer - 1] == correct_answer:
+            print("Correct!\n")
+            time.sleep(1)
+            score += 1
+        else:
+            print(f"Wrong answer. The correct answer was {correct_answer}.\n")
+            time.sleep(1)
+        return score
 
 
 def check_answer_present_simple(question_source, question, answer):
+    """
+    Checks the answer if the user is doing a present simple quiz.
+
+    :param question_source: a dictionary which contains the questions referenced for the quiz.
+    :param question: a string, the question whose answer is currently being 'graded'
+    :param answer: a string, the answer the user has given to be 'graded'
+    :return: the score for that question, an integer.
+    """
     score = 0
     correct_answer = question_source["present simple mc"]["questions"][question][0]
-    given_answer = question_source["present simple mc"]["questions"][question][1][int(answer)-1]
-    if int(answer) != 1 and int(answer) != 2 and int(answer) != 3:
-        raise ValueError
-    elif given_answer == correct_answer:
-        print("Correct!")
-        score += 1
+    try:  # code that could raise a Value Error
+        if answer != "1" and answer != "2" and answer != "3":
+            raise ValueError
+    except ValueError:
+        print("Invalid input. Input must be a number between 1 and 3.\n")
+        answer = input("Enter a valid input.\n").strip().lower()
+        return check_answer_present_simple(question_source, question, answer)
     else:
-        print(f"Wrong answer.The correct answer is {correct_answer}.")
-    return score
+        if question_source["present simple mc"]["questions"][question][1][int(answer) - 1] == correct_answer:
+            print("Correct!\n")
+            time.sleep(1)
+            score += 1
+        else:
+            print(f"Wrong answer. The correct answer was {correct_answer}.\n")
+            time.sleep(1)
+        return score
 
 
-if __name__ == '__main__':
+def main():
     grammar_test()
+
+
+if __name__ == "__main__":
+    doctest.testmod()
